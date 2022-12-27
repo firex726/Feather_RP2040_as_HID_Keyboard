61cc9609072a5a2d71d2c1c4efd40b2863cc6350
firex726 <firex726@yahoo.com>
2022-12-27 06:17:37 -0600

    Create Feather_RP_2040_as_HID_Keyboard.ino

---
+++ Feather_RP_2040_as_HID_Keyboard.ino
@@ -0,0 +1,84 @@
+#include "Adafruit_NeoKey_1x4.h"
+#include "seesaw_neopixel.h"
+#include "Keyboard.h"
+
+Adafruit_NeoKey_1x4 neokey(NEOKEY_1X4_ADDR, &Wire1); 
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Keyboard.begin();
+
+  if (! neokey.begin(0x30)) {     // begin with I2C address, default is 0x30
+  }
+
+}
+
+void loop() {
+
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  uint8_t buttons = neokey.read();
+
+    if (buttons & (1<<0)) {
+    neokey.pixels.setPixelColor(0, 0x00FFFF); // blue
+//    neokey.pixels.show();
+    Keyboard.print("kdt");
+    delay(10);
+    Keyboard.press(KEY_TAB);
+    delay(10);
+    Keyboard.releaseAll();
+    delay(10);
+    Keyboard.print("0505");
+    delay(10);
+    Keyboard.press(KEY_KP_ENTER);
+    delay(10);
+    Keyboard.releaseAll();
+    delay(100);
+  } else {
+  }
+
+    if (buttons & (1<<1)) {
+    neokey.pixels.setPixelColor(1, 0x00FF00); // green
+//    neokey.pixels.show();
+    Keyboard.print("010");
+    delay(10);
+    Keyboard.press(KEY_TAB);
+    delay(10);
+    Keyboard.releaseAll();
+    delay(100);
+  } else {
+  }
+
+    if (buttons & (1<<2)) {
+    neokey.pixels.setPixelColor(2, 0xFFFF00); // yellow
+//    neokey.pixels.show();
+    Keyboard.print("104ns");
+    delay(10);
+    Keyboard.press(KEY_TAB);
+    delay(10);
+    Keyboard.releaseAll();
+    delay(100);
+  } else {
+  }
+  
+  if (buttons & (1<<3)) {
+    neokey.pixels.setPixelColor(3, 0xFF0000); // red
+//    neokey.pixels.show();
+    Keyboard.print("279k");
+    delay(10);
+    Keyboard.press(KEY_TAB);
+    delay(10);
+    Keyboard.releaseAll();
+    delay(100);
+  } else {
+  }
+
+    Keyboard.releaseAll();
+    neokey.pixels.setPixelColor(0, 0);
+    neokey.pixels.setPixelColor(1, 0);
+    neokey.pixels.setPixelColor(2, 0);
+    neokey.pixels.setPixelColor(3, 0);
+    neokey.pixels.show();
+    delay(50);
+
+}
