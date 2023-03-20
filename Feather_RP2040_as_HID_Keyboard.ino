3b58e1af6a3f82ecb3ea53bb2fee96cb9feb0a9f
firex726 <firex726@yahoo.com>
2023-03-19 22:20:47 -0500

    Update Feather_RP2040_as_HID_Keyboard.ino

--- Feather_RP2040_as_HID_Keyboard.ino
+++ Feather_RP2040_as_HID_Keyboard.ino
@@ -1,46 +1,47 @@
+// these are libraries, dont worry about them
 #include "Adafruit_NeoKey_1x4.h"
 #include "seesaw_neopixel.h"
 #include "Keyboard.h"
 
+// this line defines what port the keybaord is connected to
 Adafruit_NeoKey_1x4 neokey(NEOKEY_1X4_ADDR, &Wire1); 
 
-void setup() {
+// these lines start the keyboard as a human interface device
+void setup() { 
   pinMode(LED_BUILTIN, OUTPUT);
   Keyboard.begin();
-
-  if (! neokey.begin(0x30)) {     // begin with I2C address, default is 0x30
-  }
+  neokey.begin(0x30);
 
 }
 
-void loop() {
+void loop() { 
+  digitalWrite(LED_BUILTIN, HIGH); 
 
-  digitalWrite(LED_BUILTIN, HIGH);
+// this line checks if a key has been pressed
+  uint8_t buttons = neokey.read(); 
 
-  uint8_t buttons = neokey.read();
-
-    if (buttons & (1<<0)) {
-    neokey.pixels.setPixelColor(0, 0x00FFFF); // blue
-//    neokey.pixels.show();
-    Keyboard.print("abc");
+    if (buttons & (1<<0)) { // if the first button is pressed it will do the following
+    neokey.pixels.setPixelColor(0, 0x00FFFF); // sets the light color to blue
+//    neokey.pixels.show(); // displays light when key pressed -disabled-
+    Keyboard.print("kdt"); // types the letters kdt, set to lower case so when the keyboard has caps lock it'll show as caps
+    delay(10); // waits 10 miliseconds for debounce
+    Keyboard.press(KEY_TAB); // presses special key TAB
     delay(10);
-    Keyboard.press(KEY_TAB);
+    Keyboard.releaseAll(); // this will release all keys, as through you took your finger off the keyboard, otherwise it'll be like you're holding TAB
+    delay(10); 
+    Keyboard.print("0505"); // types the charecters 0505
     delay(10);
-    Keyboard.releaseAll();
+    Keyboard.press(KEY_KP_ENTER); // presses special key ENTER
     delay(10);
-    Keyboard.print("1234");
-    delay(10);
-    Keyboard.press(KEY_KP_ENTER);
-    delay(10);
-    Keyboard.releaseAll();
+    Keyboard.releaseAll(); 
     delay(100);
-  } else {
+  } else { // this is where you would have the board do something when not pressing a button, in this case it does nothing. 
   }
 
     if (buttons & (1<<1)) {
     neokey.pixels.setPixelColor(1, 0x00FF00); // green
 //    neokey.pixels.show();
-    Keyboard.print("123");
+    Keyboard.print("104ns");
     delay(10);
     Keyboard.press(KEY_TAB);
     delay(10);
@@ -52,7 +53,7 @@ void loop() {
     if (buttons & (1<<2)) {
     neokey.pixels.setPixelColor(2, 0xFFFF00); // yellow
 //    neokey.pixels.show();
-    Keyboard.print("123ab");
+    Keyboard.print("299k");
     delay(10);
     Keyboard.press(KEY_TAB);
     delay(10);
@@ -64,7 +65,7 @@ void loop() {
   if (buttons & (1<<3)) {
     neokey.pixels.setPixelColor(3, 0xFF0000); // red
 //    neokey.pixels.show();
-    Keyboard.print("123a");
+    Keyboard.print("599k");
     delay(10);
     Keyboard.press(KEY_TAB);
     delay(10);
@@ -74,11 +75,11 @@ void loop() {
   }
 
     Keyboard.releaseAll();
-    neokey.pixels.setPixelColor(0, 0);
-    neokey.pixels.setPixelColor(1, 0);
-    neokey.pixels.setPixelColor(2, 0);
-    neokey.pixels.setPixelColor(3, 0);
-    neokey.pixels.show();
+    neokey.pixels.setPixelColor(0, 0); // sets key light to color off
+    neokey.pixels.setPixelColor(1, 0); // sets key light to color off
+    neokey.pixels.setPixelColor(2, 0); // sets key light to color off
+    neokey.pixels.setPixelColor(3, 0); // sets key light to color off
+    neokey.pixels.show(); // shows the light color off
     delay(50);
 
 }
